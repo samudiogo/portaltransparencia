0cc4cdc3261a414f78a000760c89140a9a145f2a
Supremo Samuel Diogo <samuel-diogo@hotmail.com>
2017-08-01 15:06:04 -0300

    criação do controle e tela gestão de pessoas

---
+++ PortalDPGE/PortalDPGE.Transparencia.Web/Controllers/GestaoPessoasController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDPGE.Transparencia.Web.Controllers
+{
+    [RoutePrefix("gestao-pessoas")]
+    public class GestaoPessoasController : Controller
+    {
+        // GET: GestaoPessoas
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("quadro-servidores-ativos")]
+        public ActionResult ListaQuadroAtivos()
+        {
+            return View();
+        }
+    }
+}
