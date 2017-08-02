0ebc4c9a26b749e804a909c3811af44364bcac08
alanvictordv <alanvictordiasvieira@gmail.com>
2017-08-02 17:35:57 -0300

    Criação de Entidade ServidorModel

---
+++ PortalDPGE/PortalDPGE.Transparencia.Web/Models/ServidorModel.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalDPGE.Transparencia.Web.Models
+{
+    public class ServidorModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        [Display(Name = "Matrícula")]
+        public string Matricula { get; set; }
+        public string Cargo { get; set; }
+        [Display(Name = "Função")]
+        public string Funcao { get; set; }
+        [Display(Name = "Lotação")]
+        public string Lotacao { get; set; }
+        [Display(Name = "Nomeação")]
+        public DateTime Nomeacao { get; set; }
+        [Display(Name = "Estável")]
+        public string Estavel { get; set; }
+        public int Antiguidade { get; set; }
+    }
+}
